6ff615d058dede08e1f0093296975e5520f719dd
Heather Nuffer <zzzuzz@gmail.com>
2019-12-19 11:53:01 -0700

    Added my line

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
         </a>
         !
       </h2>
+      <p> Here's my little change</p>
       <p>
         <a
           href="https://github.com/zeit/now-examples/tree/master/create-react-app-functions"
